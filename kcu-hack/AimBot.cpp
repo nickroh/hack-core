d8c90b8ca00eacf4d8f372f20e283badfad8bc97
GeonhoRoh <roh517@naver.com>
2025-04-20 06:24:09 -0500

    FEAT: create aimbot class

---
+++ kcu-hack/AimBot.cpp
@@ -0,0 +1,9 @@
+#include "AimBot.h"
+
+AimBot::AimBot() {
+    // Initialization code here (if any)
+}
+
+AimBot::~AimBot() {
+    // Cleanup code here (if any)
+}
---
+++ kcu-hack/include/AimBot.h
@@ -0,0 +1,12 @@
+class AimBot {
+public:
+    AimBot();   // Constructor
+    ~AimBot();  // Destructor
+
+    bool activateAimBot();
+
+private:
+    int multiplier;
+    bool activated;
+
+};
